eb987feda61cd6fde17aeaf7c29eea683074ab26
utsugiyasuhiro <okadasaikyou3532@ezweb.ne.jp>
2020-05-06 23:48:55 +0900

    new action create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,8 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @events = Event.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :events, only: :index
+  resources :events, only: [:index, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
