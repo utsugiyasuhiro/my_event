20247ab081ea4bec632b0a33bc18b9d283472ce7
utsugiyasuhiro <okadasaikyou3532@ezweb.ne.jp>
2020-05-07 14:26:02 +0900

    destroy action create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,6 +12,11 @@ class EventsController < ApplicationController
     Event.create(event_params)
   end
 
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+  end
+
   private
   def event_params
     params.require(:event).permit(:name, :image, :text)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :events, only: [:index, :new, :create] 
+  root to: 'events#index'
+  resources :events, only: [:index, :new, :create, :destroy] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
