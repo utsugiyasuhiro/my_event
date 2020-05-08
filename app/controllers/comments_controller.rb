2db7a789de0d34f48814f3d46cc026da28600358
utsugiyasuhiro <okadasaikyou3532@ezweb.ne.jp>
2020-05-08 12:55:15 +0900

    非同期通信 create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/events/#{comment.event.id}"
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to event_path(params[:event_id])  }
+      format.json
+    end
   end
 
   private
